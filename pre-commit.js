3154c6cb35ab5d3e0d9461f9a1dd69f7ea99ecc4
DritaSkeraj <dritaskeraj@gmail.com>
2024-06-11 00:58:02 +0200

    fix: pre-commit script

--- pre-commit.js
+++ pre-commit.js
@@ -1,15 +1,6 @@
-console.log("hellow. Youre in for a treat!");
+console.log("hellow. You're in for a treat!");
 
-const { execSync } = require("child_process");
-const path = require("path");
-
-console.log("=>", process.debugPort);
-
-const SHARED_TYPES_DIR = path.resolve(__dirname, "../types");
 try {
-  //   // Change to the shared types directory
-  process.chdir(SHARED_TYPES_DIR);
-  console.log("=>", process.debugPort);
   // Add any new or modified types
   execSync("git add -A", { stdio: "inherit" });
   console.log("Files staged!");
